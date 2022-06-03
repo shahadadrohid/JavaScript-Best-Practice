610d79631e0536eb210f94411cdf61eb477dd717
Mohammad Shahadad Hossain <shahadadrohid@gmail.com>
2022-06-03 22:29:38 +0600

    new commit

---
+++ function.js
@@ -0,0 +1,8 @@
+function startFan() {
+    console.log('Press the switch')
+    // return
+}
+
+startFan()
+let result = startFan()
+console.log(result)
--- while-for-loop.js
+++ while-for-loop.js
@@ -1,4 +1,4 @@
-let number = 0;
+/*let number = 0;
 while (number <= 10) {
     console.log(number);
     number++;
@@ -33,4 +33,10 @@ for (let i = 0; i <= 10; i = i + 2) {
 // Odd number by for loop 
 for (let i = 1; i <= 10; i = i + 2) {
     console.log(i)
+}*/
+
+let numbers = [49, 29, 93, 19, 84, 92, 91, 51]
+for (var i = 0; i <= numbers.length; i++) {
+    let element = numbers[i]
+    console.log(element)
 }
