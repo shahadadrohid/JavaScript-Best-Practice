9666c80b3ed3753ddaced1166ecabf3ec694ada1
Mohammad Shahadad Hossain <shahadadrohid@gmail.com>
2022-06-04 01:41:12 +0600

    new commit

--- function.js
+++ function.js
@@ -1,8 +1,29 @@
-function startFan() {
-    console.log('Press the switch')
-    // return
+// function startFan() {
+//     console.log('Press the switch')
+//     // return
+// }
+
+// startFan()
+// let result = startFan()
+// console.log(result)
+
+function buyMilk(taka) {
+    let milkPrice = 50;
+    let milkQuantity = taka / milkPrice;
+    return milkQuantity
 }
 
-startFan()
-let result = startFan()
-console.log(result)
+var milkMoney = 400;
+var milkQuantityResult = buyMilk(milkMoney);
+console.log(milkQuantityResult)
+
+// Multiple Parameter 
+function multiplyNumber(num1, num2) {
+    var total = num1 * num2;
+    return total;
+}
+
+var firstNumber = 10;
+var secondNumber = 10;
+var result = multiplyNumber(firstNumber, secondNumber);
+console.log(result);
